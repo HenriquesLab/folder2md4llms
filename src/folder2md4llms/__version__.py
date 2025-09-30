483b157e1ccbb58b2d8537cc4690eb9132f9a9d7
Ricardo Henriques <paxcalpt@gmail.com>
2025-09-30 21:14:40 +0100

    chore: bump version to 0.5.4

--- src/folder2md4llms/__version__.py
+++ src/folder2md4llms/__version__.py
@@ -4,5 +4,5 @@ This file is the single source of truth for version information.
 Do not create __about__.py - use only this __version__.py file.
 """
 
-__version__ = "0.5.3"
-__version_tuple__ = (0, 5, 3)
+__version__ = "0.5.4"
+__version_tuple__ = (0, 5, 4)
